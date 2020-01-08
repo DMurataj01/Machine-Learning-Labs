8cae96bc80d4e57e9964a26b7356e62050a14211
Denisi Murataj <dmurataj01@gmail.com>
2020-01-08 18:51:22 +0000

    lab_1 - numpy test

---
+++ lab_1.py
@@ -0,0 +1,10 @@
+#Lab_1 - Numpy Test
+
+import numpy as np
+
+x = np.array([1, 2, 3])
+y = np.array([[2], [3], [4]])
+A = np.array([[1, 2, 4],[2, 6, 8],[3, 3, 3]])
+
+print("x: {}, y: {}, A: {}".format(x.shape[0], y.shape, A.shape))
+
